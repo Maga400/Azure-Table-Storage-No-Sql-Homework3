df24b46d16e09d4bf833788ee4d747abf107f198
Hemze_jx83@itstep.edu.az <Hemze_jx83@itstep.edu.az>
2024-11-08 14:42:08 +0400

    Add project files.

---
+++ Azure Table Storage No Sql Homework3/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Azure_Table_Storage_No_Sql_Homework3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
